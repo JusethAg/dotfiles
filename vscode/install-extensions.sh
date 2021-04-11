d3faf47e86c5c1fefda4effa4c5fb3fb4b84a407
JusethAg <mauagueda@gmail.com>
2021-04-11 00:06:49 -0500

    Update zprofile and vscode extensions for including Go tools

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -4,32 +4,34 @@ echo "Installing VSCode extensions..."
 
 code -v > /dev/null
 if [[ $? -eq 0 ]];then
-  code --install-extension kisstkondoros.vscode-codemetrics
   code --install-extension aeschli.vscode-css-formatter
-  code --install-extension msjsdiag.debugger-for-chrome
-  code --install-extension PeterJausovec.vscode-docker
-  code --install-extension dracula-theme.theme-dracula
-  code --install-extension dbaeumer.vscode-eslint
-  code --install-extension nopjmp.fairyfloss
-  code --install-extension donjayamanne.githistory
-  code --install-extension eamodio.gitlens
-  code --install-extension wix.vscode-import-cost
-  code --install-extension Zignd.html-css-class-completion
   code --install-extension christian-kohler.npm-intellisense
-  code --install-extension techer.open-in-browser
-  code --install-extension pnp.polacode
-  code --install-extension nodesource.vscode-for-node-js-development-pack
-  code --install-extension robertohuertasm.vscode-icons
-  code --install-extension streetsidesoftware.code-spell-checker
-  code --install-extension samuelcolvin.jinjahtml
+  code --install-extension dbaeumer.vscode-eslint
+  code --install-extension donjayamanne.githistory
+  code --install-extension dracula-theme.theme-dracula
+  code --install-extension eamodio.gitlens
+  code --install-extension golang.go
+  code --install-extension hashicorp.terraform
+  code --install-extension ivory-lab.jenkinsfile-support
+  code --install-extension kisstkondoros.vscode-codemetrics
   code --install-extension ms-azuretools.vscode-azurefunctions
+  code --install-extension ms-azuretools.vscode-azureresourcegroups
   code --install-extension ms-azuretools.vscode-docker
   code --install-extension ms-vscode.azure-account
   code --install-extension ms-vsliveshare.vsliveshare
   code --install-extension ms-vsliveshare.vsliveshare-audio
-  code --install-extension nicolasvuillamy.vscode-groovy-lint
-  code --install-extension robbowen.synthwave-vscode
+  code --install-extension msjsdiag.debugger-for-chrome
+  code --install-extension NicolasVuillamy.vscode-groovy-lint
+  code --install-extension nodesource.vscode-for-node-js-development-pack
+  code --install-extension nopjmp.fairyfloss
+  code --install-extension pnp.polacode
+  code --install-extension RobbOwen.synthwave-vscode
+  code --install-extension samuelcolvin.jinjahtml
+  code --install-extension streetsidesoftware.code-spell-checker
+  code --install-extension techer.open-in-browser
   code --install-extension vscode-icons-team.vscode-icons
+  code --install-extension wix.vscode-import-cost
+  code --install-extension Zignd.html-css-class-completion
 
 else
   echo "Error, command 'code' doesn't exist"
